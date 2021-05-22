3ca10233c8f5af7e3d4a75a7586c910011bdab22
Kazuhiro Kato <kazuhiro.kato@hotmail.co.jp>
2021-05-22 15:32:46 +0900

    最初のファイル登録

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace ZipMake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.buttonMakeZip = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "パスワード";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(102, 23);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(115, 19);
+            this.textBoxPassword.TabIndex = 2;
+            this.textBoxPassword.TextChanged += new System.EventHandler(this.textBoxPassword_TextChanged);
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.AllowDrop = true;
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.ItemHeight = 12;
+            this.listBoxFiles.Location = new System.Drawing.Point(27, 67);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(475, 64);
+            this.listBoxFiles.TabIndex = 3;
+            this.listBoxFiles.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBoxFiles_DragDrop);
+            this.listBoxFiles.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBoxFiles_DragEnter);
+            // 
+            // buttonMakeZip
+            // 
+            this.buttonMakeZip.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonMakeZip.Location = new System.Drawing.Point(27, 148);
+            this.buttonMakeZip.Name = "buttonMakeZip";
+            this.buttonMakeZip.Size = new System.Drawing.Size(475, 44);
+            this.buttonMakeZip.TabIndex = 4;
+            this.buttonMakeZip.Text = "ZIPファイル作成";
+            this.buttonMakeZip.UseVisualStyleBackColor = true;
+            this.buttonMakeZip.Click += new System.EventHandler(this.buttonMakeZip_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(246, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ファイルをドラッグ＆ドロップして";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(248, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(217, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "「ZIPファイル作成」ボタンをクリックしてください。";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(248, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(254, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "ファイルのあるディレクトリに ZIP ファイルを作成します。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 215);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonMakeZip);
+            this.Controls.Add(this.listBoxFiles);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "暗号化ZIPファイルを作成する";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.Button buttonMakeZip;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,172 @@
+﻿using DPAPI;
+using Ionic.Zip;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZipMake
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.DocPassword != null)
+            {
+                try
+                {
+                    this.textBoxPassword.Text = DecryptDPAPI(Properties.Settings.Default.DocPassword);
+                }
+                catch(Exception exp)
+                {
+                    MessageBox.Show("パスワードの復元に失敗しました。もう一度パスワードを設定してください。");
+                }
+            }
+
+            textChangeFlg = false; // パスワード変更フラグ
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (textChangeFlg)
+            {
+                Properties.Settings.Default.DocPassword = EncryptDPAPI(this.textBoxPassword.Text);
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        string EncryptDPAPI(string s)
+        {
+            DataProtector dp = new DataProtector(
+                DataProtector.Store.USE_MACHINE_STORE);
+            byte[] dataToEncrypt =
+                Encoding.UTF8.GetBytes(s);
+            // Not passing optional entropy in this example
+            // Could pass random value (stored by the application) for added
+            // security when using DPAPI with the machine store.
+            return Convert.ToBase64String(dp.Encrypt(dataToEncrypt, null));
+        }
+
+        string DecryptDPAPI(string enc)
+        {
+            DataProtector dp = new
+                    DataProtector(DataProtector.Store.USE_MACHINE_STORE);
+            byte[] dataToDecrypt =
+                Convert.FromBase64String(enc);
+            // Optional entropy parameter is null. 
+            // If entropy was used within the Encrypt method, the same entropy
+            // parameter must be supplied here
+            return Encoding.UTF8.GetString(dp.Decrypt(dataToDecrypt, null));
+        }
+
+        private void buttonMakeZip_Click(object sender, EventArgs e)
+        {
+            // 入力ファイル一覧
+            var fileInfo = this.listBoxFiles.Items.Cast<ListBoxItemFile>().FirstOrDefault();
+
+            if (fileInfo == null)
+            {
+                MessageBox.Show("ファイルがありません。");
+                return;
+            }
+
+            string dir = System.IO.Path.GetDirectoryName(fileInfo.FullFileName);
+            string makeZipFileName = System.IO.Path.Combine(
+                dir,
+                System.IO.Path.GetFileNameWithoutExtension(fileInfo.FullFileName) + ".zip");
+
+            // すでに ZIP ファイルが存在する場合はエラーメッセージを出して処理を終了する。
+           if (System.IO.File.Exists(makeZipFileName))
+            {
+                MessageBox.Show("すでに ZIP ファイルが存在するため ZIPファイルは作れません。");
+                return;
+            }
+
+            // 最初のファイルがあるディレクトリに ZIP ファイルを作成する。
+            try
+            {
+                //(1)ZIPクラスをインスタンス化
+                using (ZipFile zip = new ZipFile(Encoding.GetEncoding("shift_jis")))
+                {
+                    //(2)圧縮レベルを設定
+                    zip.CompressionLevel = Ionic.Zlib.CompressionLevel.BestCompression;
+
+                    if (this.textBoxPassword.Text.Length > 0)
+                    {
+                        // パスワードがある場合は設定する
+                        zip.Password = this.textBoxPassword.Text;
+                    }
+
+                    foreach (var file in this.listBoxFiles.Items.Cast<ListBoxItemFile>())
+                    {
+                        //(3)ファイルを追加
+                        zip.AddFile(file.FullFileName, "");
+
+                    }
+                    //(5)ZIPファイルを保存
+                    zip.Save(makeZipFileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ZIP作成失敗");
+            }
+
+
+        }
+
+        private void listBoxFiles_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void listBoxFiles_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                foreach (string fileName
+                             in (string[])e.Data.GetData(DataFormats.FileDrop))
+                {
+                    ListBoxItemFile item = new ListBoxItemFile()
+                    {
+                        FileName = System.IO.Path.GetFileName(fileName),
+                        FullFileName = fileName,
+                    };
+
+                    this.listBoxFiles.Items.Add(item);
+                }
+            }
+        }
+
+        class ListBoxItemFile
+        {
+            public string FileName { get; set; }
+
+            public string FullFileName { get; set; }
+
+            public override string ToString()
+            {
+                return FileName;
+            }
+        }
+
+        bool textChangeFlg = false;
+
+        private void textBoxPassword_TextChanged(object sender, EventArgs e)
+        {
+            textChangeFlg = true;
+        }
+
+    }
+}
